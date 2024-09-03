8dfc6fe9404a494c584c0f79a16f47757a9e4fdb
ntokozo-s <ntokozomokoena655@gmail.com>
2024-09-03 22:27:10 +0200

    Added forecast API Integration

--- src/index.js
+++ src/index.js
@@ -19,6 +19,8 @@ function updateWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   wind.innerHTML = `${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="icon" />`;
+
+  getForecast(response.data.city);
 }
 function formatDate(date) {
   let minutes = date.getMinutes();
@@ -49,6 +51,43 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   searchCity(searchInputElement.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "8tca415f35dad4af35ac2o5bfe7061a9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecast = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecast =
+        forecast +
+        `<div  class="weekly-forecast"><div class="forecast-date">${formatDay(
+          day.time
+        )}</div>
+                < img src=${day.condition.icon_url} class="forecast-icon" />
+                <div class="forecast-temperature">
+                    <div class="maximum-temp"><strong>${Math.round(
+                      day.temperature.maximum
+                    )}℃</strong> </div>
+                    <div class="minimum-temp">${Math.round(
+                      day.temperature.minimum
+                    )}℃</div></div>
+                </div>`;
+    }
+  });
+  let weatherForecastElement = document.querySelector("#weather-forecast");
+  weatherForecastElement.innerHTML = forecast;
 }
 
 let searchFormElement = document.querySelector("#search-form");
